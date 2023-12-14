acb8d200411f2d4e8bc5a78c771c87c76cb06614
llz <1959657881@qq.com>
2023-12-14 21:37:19 +0800

    Revert "Initial commit"
    This reverts commit 9df88a80c656c5ed0b0bfc528cb7ff409bb0c592.

--- src/main/java/com/example/demo2_sp/repository/UserTypeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo2_sp.repository;
-
-import com.example.demo2_sp.entity.UserType.UserType;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserTypeRepository extends JpaRepository<UserType, Long> {
-    UserType findByUID(int UID);
-}
